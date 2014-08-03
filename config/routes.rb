62de2e5d0c9761cf5caeeffcc7299e582b5ad79e
muaad <muadh24@gmail.com>
2014-08-03 23:43:35 +0300

    routes for sign_up and add_members

--- config/routes.rb
+++ config/routes.rb
@@ -22,11 +22,16 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   root to: 'visitors#index'
 
+  match 'sign_up', to: 'home#sign_up', as: 'sign_up', via: "post"
+  match 'login', to: 'home#add_members', as: 'add_members', via: "post"
+
   match 'login', to: 'sessions#create', as: 'login', via: "post"
+
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'services/:id/companies', to: 'services#companies', as: 'service_companies'
   get 'companies/:id/branches', to: 'companies#branches', as: 'company_branches'
   get 'companies/:id/nearest_branch', to: 'companies#nearest_branch', as: 'company_nearest_branch'
   get 'groups/:id/members', to:'groups#members',as:'group_members'
+  
   match 'verify', to: 'users#verify', as: 'verify', via: "post"
 end
