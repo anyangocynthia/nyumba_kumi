2d64526eaecd3124230a296f17f25863a559a687
muaad <muadh24@gmail.com>
2014-11-06 15:22:07 +0300

    added some map to the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+	# layout 'dashboard'
   def index
   end
 end
--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -69,6 +69,6 @@ class IncidentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def incident_params
-      params.require(:incident).permit(:type, :user_id, :notification_id, :location, :resolved, :false_flag)
+      params.require(:incident).permit(:incident_type, :user_id, :notification_id, :location, :resolved, :false_flag)
     end
 end
