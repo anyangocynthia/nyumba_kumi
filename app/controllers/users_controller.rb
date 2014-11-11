487cb33d362a20ea8c1642295837ad42f1928741
muaad <muadh24@gmail.com>
2014-11-11 19:27:21 +0300

    add house_name, house_number and photo to user#show response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,9 @@ class UsersController < ApplicationController
     user[:group_id] = @user.group_id
     user[:in_a_group] = !@user.group_id.nil?
     user[:user_type] = @user.user_type
+    user[:house_name] = House.find(@user.house_id).house_name
+    user[:house_number] = @user.house_number
+    user[:photo] = @user.photo.url
     user[:member_since] = "#{@user.created_at.strftime("%d/%m/%Y")} #{@user.created_at.strftime("%I:%M%p")}"
 
     render json: user
