52db22f82261764b31944653b196f2c988e08e64
muaad <muadh24@gmail.com>
2014-12-09 21:51:16 +0300

    added back validation for photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   has_many :devices
 
   has_attached_file :photo#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  # validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   def normalize_number
 		self.phone_number = PhonyRails.normalize_number PhonyRails.normalize_number(self.phone_number), country_number: "254"
