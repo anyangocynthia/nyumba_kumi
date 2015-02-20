13de52c9d0cebc7347421fb84ee6f2ce81832509
muaad <muadh24@gmail.com>
2015-02-20 06:55:21 +0300

    added photo to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
   has_many :companies
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220032557) do
+ActiveRecord::Schema.define(version: 20150220035231) do
 
   create_table "appartment_residents", force: true do |t|
     t.integer  "appartment_id"
@@ -217,6 +217,10 @@ ActiveRecord::Schema.define(version: 20150220032557) do
     t.datetime "updated_at"
     t.string   "name"
     t.boolean  "is_admin"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
