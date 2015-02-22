19894c366df966f90ab3b78caf0ab8c721d793bb
muaad <muadh24@gmail.com>
2015-02-22 15:21:11 +0300

    styling blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  layout 'blog'
 
   # GET /articles
   # GET /articles.json
---
+++ config/initializers/assets.rb
@@ -0,0 +1,2 @@
+Rails.application.config.assets.precompile += %w( blog.css )
+Rails.application.config.assets.precompile += %w( blog.js )
