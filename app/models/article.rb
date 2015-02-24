5f970e96f67191d08a72202fd4b69b0b245369d5
muaad <muadh24@gmail.com>
2015-02-24 15:00:07 +0300

    shorthand methods for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 class Article < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+
+  def summary
+  	self.body.split(".")[0..3].join(" ")
+  end
+
+  def author
+  	user.name
+  end
 end
