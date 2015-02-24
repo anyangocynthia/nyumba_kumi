e85fa65a137518ebdd4c744c6786b3b0d0123dc5
muaad <muadh24@gmail.com>
2015-02-24 15:09:53 +0300

    add a feature image to article when creating or editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -88,6 +88,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body, :slug, :category_id, :published)
+      params.require(:article).permit(:title, :body, :slug, :category_id, :published, :feature_image)
     end
 end
