a4c4e68cfdf14a54c7e6376c6c3f2a03637f8818
muaad <muadh24@gmail.com>
2015-02-28 17:07:50 +0300

    added email to account

--- db/migrate/20150228134317_create_accounts.rb
+++ db/migrate/20150228134317_create_accounts.rb
@@ -1,6 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
+      t.string :email
       t.string :name
       t.references :user, index: true
       t.boolean :setup, default: false
