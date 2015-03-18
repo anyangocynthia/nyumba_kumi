3ea55b432437c718ad8e11bfb0c8c3fac6643cd6
anyangocynthia <acynthiaanyango@gmail.com>
2015-03-18 13:57:20 +0300

    put some padding

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  # validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   has_many :companies
   has_many :articles
