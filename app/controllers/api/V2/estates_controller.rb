748043617be5c0a70ac25ffd1b6b07df2d5a4719
muaad <muadh24@gmail.com>
2015-03-19 00:55:36 +0300

    fix some syntax error

--- app/controllers/api/V2/estates_controller.rb
+++ app/controllers/api/V2/estates_controller.rb
@@ -1,7 +1,7 @@
 module Api::V2
 	class EstatesController < ApplicationController
 		def nearby_estates
-			render json: Estate.find_nearest params[:latitude], params[:longitude], 20
+			render json: Estate.find_nearest(params[:latitude], params[:longitude], 20)
 		end
 
 		def index
